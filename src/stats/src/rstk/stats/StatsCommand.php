c6a02fdce70d9dfd51f1345defebe54584a5fab9
rstk <rstkdev@gmail.com>
2021-07-07 18:02:45 -0400

    stats command

--- src/stats/src/rstk/stats/StatsCommand.php
+++ src/stats/src/rstk/stats/StatsCommand.php
@@ -8,6 +8,8 @@ use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\plugin\Plugin;
 use pocketmine\command\PluginIdentifiableCommand;
+use pocketmine\Player;
+use pocketmine\utils\TextFormat;
 
 class StatsCommand extends Command implements PluginIdentifiableCommand {
     
@@ -16,16 +18,15 @@ class StatsCommand extends Command implements PluginIdentifiableCommand {
      */
     private $plugin;
 
-    public function __construct(string $name, Plugin $plugin, string $description = "", ?string $usageMessage = null, array $aliases = []) {
+    public function __construct(string $name, Plugin $plugin) {
         parent::__construct(
             $name,
-            $description,
-            $usageMessage,
-            $aliases
+            "Show server stats",
+            "/stats"
         );
 
         $this->plugin = $plugin;
-        $this->setDescription($description);
+        //$this->setDescription();
     }
 
     public function getPlugin(): Plugin {
@@ -33,6 +34,58 @@ class StatsCommand extends Command implements PluginIdentifiableCommand {
     }
 
     public function execute(CommandSender $sender, string $commandLabel, array $args) {
-        $sender->sendMessage("stats command ran");
+        $server = $sender->getServer();
+
+        $tpsnow = $server->getTicksPerSecond();
+        $tpsavrg = $server->getTicksPerSecondAverage();
+        $playercount = count($server->getOnlinePlayers());
+
+        $sender->sendMessage(TextFormat::RED . "=====" . TextFormat::WHITE . " SERVER STATS " . TextFormat::RED . "=====");
+        
+        $s = $playercount > 1 ? "s" : "";
+        $sender->sendMessage(
+            TextFormat::GREEN . TextFormat::BOLD . strval($playercount) .
+            TextFormat::RESET . TextFormat::GREEN . "player$s online"
+        );
+
+        if ($sender instanceof Player) {
+            $ping = $sender->getPing();
+            $sender->sendMessage(
+                TextFormat::BOLD . "Ping: " .
+                TextFormat::RESET . $this->getPingColor($ping) . strval($ping)
+            );
+        }
+
+        $sender->sendMessage(
+            TextFormat::BOLD . "TPS: " .
+            TextFormat::RESET . $this->getTpsColor($tpsnow) . strval($tpsnow)
+        );
+
+        $sender->sendMessage(
+            TextFormat::BOLD . "Avrg TPS: " .
+            TextFormat::RESET . $this->getTpsColor($tpsavrg) . strval($tpsavrg)
+        );
+
+        $sender->sendMessage(TextFormat::RED . "======================");
+    }
+
+    private function getTpsColor(float $tps): string {
+        if ($tps > 18) {
+            return TextFormat::GREEN;
+        } elseif ($tps > 15) {
+            return TextFormat::YELLOW;
+        } else {
+            return TextFormat::RED;
+        }
+    }
+    
+    private function getPingColor(int $ping): string {
+        if ($ping <= 50) {
+            return TextFormat::GREEN;
+        } elseif ($ping <= 100) {
+            return TextFormat::YELLOW;
+        } else {
+            return TextFormat::RED;
+        }
     }
 }
