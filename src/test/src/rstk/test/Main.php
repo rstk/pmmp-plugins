c6122deecea8c93e1a89fe8083fbc1e7e426f847
rstk <rstkdev@gmail.com>
2021-07-07 01:42:26 -0400

    add plugin.yml to /test

--- src/test/src/rstk/test/Main.php
+++ src/test/src/rstk/test/Main.php
@@ -7,5 +7,11 @@ namespace rstk\test;
 use pocketmine\plugin\PluginBase;
 
 class Main extends PluginBase {
-    
+    public function onEnable() {
+        $this->getLogger()->info("Test plugin started!");
+    }
+
+    public function onLoad() {
+        $this->getLogger()->info("Test plugin loaded!");
+    }
 }
